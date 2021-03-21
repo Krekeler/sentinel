c295ba69971531bb32d168b267414f760834af1f
Harald Krekeler <mail@krekeler.de>
2021-03-21 19:39:46 +0100

    Dash to DMS: replace texts

--- share/travis_setup.sh
+++ share/travis_setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -evx
 
-mkdir ~/.dashcore
+mkdir ~/.dmscore
 
 # safety check
-if [ ! -f ~/.dashcore/.dash.conf ]; then
-  cp share/dash.conf.example ~/.dashcore/dash.conf
+if [ ! -f ~/.dmscore/.dms.conf ]; then
+  cp share/dms.conf.example ~/.dmscore/dms.conf
 fi
